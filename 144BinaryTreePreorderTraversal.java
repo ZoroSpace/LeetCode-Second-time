1818ad116ec3bc45ec9f202144b70ccc36e60c06
Zoro <ZoroSpace@users.noreply.github.com>
2018-05-27 23:22:49 +0800

    Update 144BinaryTreePreorderTraversal.java

--- 144BinaryTreePreorderTraversal.java
+++ 144BinaryTreePreorderTraversal.java
@@ -21,3 +21,40 @@ class Solution {
         return result;
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+	public List<Integer> preorderTraversal(TreeNode root) {
+        LinkedList<TreeNode> stack = new LinkedList<>();
+		LinkedList<Integer> result = new LinkedList<>();
+		while(root != null) {
+			stack.offerLast(root);
+			result.add(root.val);
+			root = root.left;
+		}
+		while(stack.size() != 0) {
+			root = stack.peekLast();
+			if(root != null && root.right != null) {
+				stack.pollLast();
+				root = root.right;
+				while(root != null) {
+					stack.offerLast(root);
+					result.add(root.val);
+					root = root.left;
+				}
+			} else {
+				stack.pollLast();
+			}
+		}
+		return result;
+	}
+}
