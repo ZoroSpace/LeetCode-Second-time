5f6c16005de0d87bea00d174702ba1c98409d80a
Zoro <ZoroSpace@users.noreply.github.com>
2018-05-31 20:32:21 +0800

    Update 078Subsets.java

--- 078Subsets.java
+++ 078Subsets.java
@@ -20,3 +20,20 @@ class Solution {
         return result;
     }
 }
+
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new LinkedList<>();
+        helper(ans,new LinkedList<Integer>(),nums,0);
+        return ans;
+    }
+    void helper(List<List<Integer>> ans,LinkedList<Integer> path,int[] nums,int start) {
+        ans.add(new LinkedList<Integer>(path));
+        for(int i = start;i < nums.length;i++) {
+            path.add(nums[i]);
+            helper(ans,path,nums,i+1);
+            path.remove(path.size()-1);
+        }
+    }
+}
