c8ac93dbfb1d8196a4f17be0dcde20a3057dd7df
Zoro <ZoroSpace@users.noreply.github.com>
2018-06-09 23:22:13 +0800

    Update 145BinaryTreePostorderTraversal.java

--- 145BinaryTreePostorderTraversal.java
+++ 145BinaryTreePostorderTraversal.java
@@ -13,13 +13,44 @@ class Solution {
         helper(ans,root);
         return ans;
     }
-    
     void helper(List<Integer> ans,TreeNode root) {
         if(root == null) return;
         helper(ans,root.left);
         helper(ans,root.right);
         ans.add(root.val);
     }
-    
-    
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> ans = new LinkedList<>();
+        if(root == null) return ans;
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        while(root != null) {
+            stack.offerLast(root);
+            ans.offerFirst(root.val);
+            root = root.right;
+        }
+        while(stack.size() != 0) {
+            root = stack.pollLast();
+            if(root.left != null) {
+                root = root.left;
+                while(root != null) {
+                    stack.offerLast(root);
+                    ans.offerFirst(root.val);
+                    root = root.right;
+                }
+            }
+        }
+        return ans;
+    }
 }
