8e03e71a7aaa75760b6b449d3229d616d211b861
Zoro <ZoroSpace@users.noreply.github.com>
2018-06-12 20:55:36 +0800

    Update 135Candy.java

--- 135Candy.java
+++ 135Candy.java
@@ -27,3 +27,22 @@ class Solution {
         return ans;
     }
 }
+class Solution {
+    public int candy(int[] r) {
+        int n = r.length;
+        // if(n == 1) return 1;
+        int[] can = new int[n];
+        for(int i = 0;i < n;i++) can[i] = 1;
+        for(int i = 1;i < n;i++) {
+            if(r[i] > r[i-1]) can[i] = can[i-1]+1;
+        }
+        for(int i = n-2;i >= 0;i--) {
+            if(r[i] > r[i+1]) can[i] = Math.max(can[i],can[i+1]+1);
+        }
+        int ans = 0;
+        for(int i = 0;i < n;i++) {
+            ans = ans+can[i];
+        }
+        return ans;
+    }
+}
