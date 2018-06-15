92e1b8f6f186539c6c02ceddfe2c44095f50afb6
Zoro <ZoroSpace@users.noreply.github.com>
2018-06-15 11:12:43 +0800

    Update 076MinimumWindowSubstring.java

--- 076MinimumWindowSubstring.java
+++ 076MinimumWindowSubstring.java
@@ -41,3 +41,39 @@ class Solution {
         return hi == s.length()?"":s.substring(lo,hi+1);
     }
 }
+
+class Solution {
+    public String minWindow(String s, String t) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        int counter = 0;
+        for(int i = 0;i < t.length();i++) {
+            if(map.containsKey(t.charAt(i))) {
+                map.put(t.charAt(i),map.get(t.charAt(i)) + 1);
+            } else map.put(t.charAt(i),1);
+            counter++;
+        }
+        int anslo = 0,anshi = s.length()+1;
+        int lo = 0,hi = 0;
+        while(hi < s.length()) {
+            char c = s.charAt(hi);
+            if(map.containsKey(c)) {
+                map.put(c,map.get(c)-1);
+                if(map.get(c) >= 0) counter--;
+            }
+            hi++;
+            while(counter == 0) {
+                if(hi - lo < anshi - anslo) {
+                    anshi = hi;
+                    anslo = lo;
+                }
+                char clo = s.charAt(lo);
+                if(map.containsKey(clo)) {
+                    map.put(clo,map.get(clo)+1);
+                    if(map.get(clo) > 0) counter++;
+                }
+                lo++;
+            }
+        }
+        return anshi-anslo == s.length()+1 ? "":s.substring(anslo,anshi);
+    }
+}
